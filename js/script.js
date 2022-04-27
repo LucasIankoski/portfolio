de2d92300fc29dfbefcb7f5b0c62cd75382c00f7
Lucas Iankoski <lucas.iankoski@outlook.com>
2022-04-27 13:42:52 -0300

    atualização da página de projetos

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function zoomProjeto(){
+    var projeto = document.getElementById('projeto');
+    const popup = document.querySelector('.popup-projeto');
+    popup.style.display = 'block';
+    
+}
+
+function fecharZoomProjeto(){    
+    const popup = document.querySelector('.popup-projeto');
+    popup.style.display = 'none';
+}
